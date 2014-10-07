7bc6727d31ae896bd0742fc6b39199d151dfa553
Juan Hernández Hernández <janukato@gmail.com>
2014-10-07 21:00:00 +0100

    Added aditional tests with a diferente machine definition.

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@
 A="a"
 B="aaabbaab"
 C="aabbbbabbbabbab"
+D="ab"
+E="aaaabbbbbbbbbbbb"
 
 build/turingmachine "test_definition" $A
 if [ $? = 0 ]; then
@@ -17,6 +19,25 @@ else
 	echo invalid string
 fi
 build/turingmachine "test_definition" $C
+if [ $? = 0 ]; then
+	echo valid string
+else
+	echo invalid string
+fi
+
+build/turingmachine "test_definition2" $A
+if [ $? = 0 ]; then
+	echo valid string
+else
+	echo invalid string
+fi
+build/turingmachine "test_definition2" $D
+if [ $? = 0 ]; then
+	echo valid string
+else
+	echo invalid string
+fi
+build/turingmachine "test_definition2" $E
 if [ $? = 0 ]; then
 	echo valid string
 else
